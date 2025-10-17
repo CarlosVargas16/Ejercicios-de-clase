9a604091c15b281c2b23ee2f5152d05ad91da9ac
CarlosVargas16 <carvargasr16@gmail.com>
2025-10-17 13:28:21 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,17 @@
+/* Vargas Rodriguez Carlos Antonio
+Ejercicio 1*/
+
+#include <stdio.h>
+
+void main()
+{
+ int main;
+  printf ("Dame un numero: \n");
+  scanf ("%d", &num);
+
+  if (num < 100)
+  {
+    printf ("El numero es mayor a 100");
+  }
+}
+  
