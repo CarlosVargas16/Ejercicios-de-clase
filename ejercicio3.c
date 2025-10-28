268ee7d0880278c7ae4b29a0faff1cc4174c09e5
CarlosVargas16 <carvargasr16@gmail.com>
2025-10-27 21:37:11 -0600

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,2 +1,26 @@
 /* Vargas Rodriguez Carlos Antonio
-Ejercicio 3*/
+ejercicio 3*/
+
+#include <stdio.h>
+
+int main()
+{
+  int num;
+  printf ("Dame un numero: \n");
+  scanf ("%d", &num);
+
+  if(num % 2==0)
+  {
+    printf("El numero es par \n");
+  
+    if(num % 3==0)
+    {
+      printf("El numero es multiplo de 3");
+    }
+  }
+  else
+  {
+    printf("El numero es impar");
+  }
+  return 0;
+}
